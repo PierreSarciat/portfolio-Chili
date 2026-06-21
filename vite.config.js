ffcc054e94ce80d2951b5982820591176b654288
PierreSarciat <phsarciat@mailo.com>
2026-06-21 16:37:22 +0200

    routes photo:id

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   plugins: [
     react(),
+     ViteImageOptimizer({
+      test: /\.(webp)$/i, // Cible uniquement les WebP générés
+      include: ['**/public/assets/images/**/thumb/**'],
+      exclude: ['node_modules'],
+      webp: { quality: 80 }, // Ré-optimisation (optionnel)
+      quiet: true,
+    }),
   ],
    resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
+      '@data': path.resolve(__dirname, './src/data'),
         },
   },
  
