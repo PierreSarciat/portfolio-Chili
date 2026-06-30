813c05f145b34db8eeed1638105d533592c4f381
PierreSarciat <phsarciat@mailo.com>
2026-06-30 09:56:58 +0200

    mise en place navbar

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -4,50 +4,71 @@ import CompassRose from "@assets/icons/rose-des-vents.png";
 
 export default function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <div className="incipit">
-                        <div className="logo">
-                            <img src={CompassRose} alt="Rose des Vents" className="logo" />
-                        </div>
-                        <h1> Chili</h1>
-                    </div>
-                    <div>
-                        Terres de contrastes
-                    </div>
-                </li>
+        <header className="navbar-container">
+            {/* Section Incipit : Logo + Titre */}
+            <div className="incipit">
+                <div className="logo-container">
+                    <img src={CompassRose} alt="Rose des Vents" className="logo" />
+                </div>
+                <div className="logo-texte">
+                    <h1>CHILI</h1>
+                    <p>Terres de contrastes</p>
+                </div>
+            </div>
 
-                <li>
-                    <NavLink to="/">
-                        ACCUEIL
-                    </NavLink>
-                </li>
-
-                <li>
-                    <NavLink to="/region/chiloe">
-                        Chiloé
-                    </NavLink>
-                </li>
-
-                <li>
-                    <NavLink to="/region/patagonie">
-                        Patagonie
-                    </NavLink>
-                </li>
-
-                <li>
-                    <NavLink to="/region/puertowilliams">
-                        Puerto Williams
-                    </NavLink>
-                </li>
-
-                <li>
-                    <NavLink to="/region/torresdelpaine">
-                        Torres del Paine
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
+            {/* Barre de navigation */}
+            <nav className="navbar">
+                <ul className="navbar-links">
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) => (isActive ? "active" : "")}
+                        >
+                            ACCUEIL
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to=""
+                            className={({ isActive }) => (isActive ? "active" : "")}
+                        >
+                            GALERIE
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/region/chiloe"
+                            className={({ isActive }) => (isActive ? "active" : "")}
+                        >
+                            CHILOE
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/region/patagonie"
+                            className={({ isActive }) => (isActive ? "active" : "")}
+                        >
+                            PATAGONIE
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/region/puertowilliams"
+                            className={({ isActive }) => (isActive ? "active" : "")}
+                        >
+                            PUERTO WILLIAMS
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/region/torresdelpaine"
+                            className={({ isActive }) => (isActive ? "active" : "")}
+                        >
+                            TORRES DEL PAINE
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 }
