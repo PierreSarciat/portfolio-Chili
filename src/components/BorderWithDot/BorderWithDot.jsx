a10c6b6d7a0bd70562f833fe1c8b1c0f4b7d2f54
PierreSarciat <phsarciat@mailo.com>
2026-07-06 21:28:37 +0200

    mise en place composant border

--- src/components/BorderWithDot/BorderWithDot.jsx
+++ src/components/BorderWithDot/BorderWithDot.jsx
@@ -2,30 +2,13 @@ import React from "react";
 import "./BorderWithDot.scss";
 
 const BorderWithDot = ({
-    color = "#3A312C",       // Couleur de la bordure (sépia par défaut)
-    dotColor = "#B89A73",   // Couleur du point (sépia clair par défaut)
-    dotSize = "8px",        // Taille du point
-    borderWidth = "1px",    // Épaisseur de la bordure
-    className = "",         // Classe CSS supplémentaire
-    children,              // Contenu à afficher au-dessus de la bordure
+    className = "",
+    children,
 }) => {
     return (
         <div className={`border-with-dot ${className}`}>
             {children}
-            <div
-                className="border-with-dot__border"
-                style={{
-                    "--border-color": color,
-                    "--border-width": borderWidth,
-                }}
-            />
-            <div
-                className="border-with-dot__dot"
-                style={{
-                    "--dot-color": dotColor,
-                    "--dot-size": dotSize,
-                }}
-            />
+            <div className="border-with-dot__border" />
         </div>
     );
 };
