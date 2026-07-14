478da7d58aa64d86f2630196c22caf33522843e0
PierreSarciat <phsarciat@mailo.com>
2026-07-14 17:41:39 +0200

    modification carte

--- src/components/map/InteractiveMap.jsx
+++ src/components/map/InteractiveMap.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './InteractiveMap.scss';
+import chileMap from '@assets/images/chile-map/background.png';
 
 export function InteractiveMap() {
-    const backgroundImageUrl = 'https://images.unsplash.com/photo-1713862032419-95a24b005dbe?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjaGlsZSUyMHZpbnRhZ2UlMjBtYXB8ZW58MXx8fHwxNzgwNDMxMTUzfDA&ixlib=rb-4.1.0&q=80&w=1080';
+    /* const backgroundImageUrl = 'https://images.unsplash.com/photo-1713862032419-95a24b005dbe?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjaGlsZSUyMHZpbnRhZ2UlMjBtYXB8ZW58MXx8fHwxNzgwNDMxMTUzfDA&ixlib=rb-4.1.0&q=80&w=1080';*/
 
     const regions = [
         { id: 'chiloe', name: 'Chiloé', x: '25%', y: '15%' },
@@ -15,25 +16,31 @@ export function InteractiveMap() {
     return (
         <div className="interactive-map-outer-border">
             <div className="interactive-map-inner-border">
-                <div
-                    className="interactive-map-container"
-                    style={{ backgroundImage: `url(${backgroundImageUrl})` }}
-                >
-                    {regions.map((region) => (
-                        <NavLink
-                            key={region.id}
-                            to={`/region/${region.id}`}
-                            className="region-label-container"
-                            style={{
-                                position: 'absolute',
-                                left: region.x,
-                                top: region.y,
-                                transform: 'translate(-50%, -50%)',
-                            }}
-                        >
-                            <span className="region-label">{region.name}</span>
-                        </NavLink>
-                    ))}
+                <div className="interactive-map-container">
+                    {/* Image de fond avec filtres pastels */}
+                    <img
+                        src={chileMap}
+                        alt="Carte du Chili"
+                        className="chile-map-image"
+                    />
+                    {/* Régions cliquables */}
+                    <div className="regions-overlay">
+                        {regions.map((region) => (
+                            <NavLink
+                                key={region.id}
+                                to={`/region/${region.id}`}
+                                className="region-label-container"
+                                style={{
+                                    position: 'absolute',
+                                    left: region.x,
+                                    top: region.y,
+                                    transform: 'translate(-50%, -50%)',
+                                }}
+                            >
+                                <span className="region-label">{region.name}</span>
+                            </NavLink>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
