6b4e3aa1c9ae9b4250154005084da462c9d6022d
PierreSarciat <phsarciat@mailo.com>
2026-08-22 21:25:09 +0200

    page accueil

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,21 +1,32 @@
 import { InteractiveMap } from '@/components/map/InteractiveMap';
 import BorderWithDot from "@components/BorderWithDot/BorderWithDot";
+import Carte from "@assets/icons/carte/carte.svg";
 import "./Home.scss"
 
 function Home() {
+
     return (
         <div className="global">
-            <section>
+            <section className='presentation'>
 
 
                 <h1>CARNETS DU CHILI</h1>
+
+                <BorderWithDot variant="title"></BorderWithDot>
                 <p>
-                    Terres de contrastes
+                    Sur plus de 4 300 kilomètres, le Chili déploie une symphonie de paysages à couper le souffle : déserts arides, fjords mystérieux, volcans majestueux et steppes infinies du Sud.
                 </p>
+
+
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Adipiscing elit quisque faucibus ex sapien vitae pellentesque. Vitae pellentesque sem placerat in id cursus mi. Cursus mi pretium tellus duis convallis tempus leo. Tempus leo eu aenean sed diam urna tempor. Urna tempor pulvinar vivamus fringilla lacus nec metus.
+                    Du désert d'Atacama, l'un des plus secs au monde, à la Terre de Feu, en passant par la cordillère des Andes et l'île enchantée de Chiloé, ce pays austral incarne l'eau, le feu et la glace dans une harmonie parfaite.
                 </p>
-                <p>Explorez le Chili</p>
+
+
+                <p>
+                    Sa géographie, d'une singularité envoûtante, semble tout droit sortie de l'imagination d'un artiste qui aurait cédé aux élans les plus fous de sa créativité.
+
+                </p>
                 <BorderWithDot variant="home"></BorderWithDot>
             </section>
 
@@ -24,8 +35,15 @@ function Home() {
                 <InteractiveMap />
             </section>
 
-            <section>
-                <p>Survolez une région pour afficher sa description.</p>
+            <section className='description'>
+                <div className="logo-carte">
+                    <img
+                        src={Carte}
+                        alt="icône carte"
+                        className=""
+                    />
+                </div>
+                <p>Survolez un point sur la carte pour afficher sa description.</p>
             </section>
         </div>
     );
