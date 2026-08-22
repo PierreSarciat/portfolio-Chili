458b3578e0d8e06c33afa5466d13e8019900088f
PierreSarciat <phsarciat@mailo.com>
2026-08-22 21:27:47 +0200

    delete alias hook

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,6 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, './src/pages'),
       '@data': path.resolve(__dirname, './src/data'),
       '@assets': path.resolve(__dirname, './src/assets'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
         },
   },
  
